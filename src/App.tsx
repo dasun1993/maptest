4c4685094ab3229d5525c1c9a6224235f20564f5
dasun <dasun@mi-synergy.com>
2020-11-08 21:35:50 +0530

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {GoogleMap, useLoadScript, Marker, InfoWindow} from "@react-google-maps/api";
+import {Libraries} from "@react-google-maps/api/dist/utils/make-load-script-url";
+import Mapstyles from "./MapStyles"
+
+const libraries: Libraries = ["places"];
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh'
 }
 
-export default App;
+const mapCenter = {
+    lat: 6.927079,
+    lng: 79.861244
+}
+
+const options = {
+    styles: Mapstyles,
+    disableDefaultUI: true,
+    zoomControl: true
+
+}
+
+interface Imarker {
+    lat: number,
+    lng: number,
+    time: any
+}
+
+
+export default function App(): JSX.Element {
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: "AIzaSyAE0DYz-oyNFJnEZM0mU7K1ZBpOQ2MygCo",
+        libraries
+    });
+
+    const [marker, setMarker] = React.useState<Imarker[]>([]);
+
+    if (loadError) return <div>erro</div>;
+    if (!isLoaded) return <div>loading ffff</div>;
+
+
+    return <div className="App">
+        <h1>Wine Shop Locator </h1>
+        <GoogleMap mapContainerStyle={mapContainerStyle} zoom={12} center={mapCenter} options={options}
+                   onClick={(e) => {
+                       setMarker(current => [...current, {
+                           lat: e.latLng.lat(),
+                           lng: e.latLng.lng(),
+                           time: new Date()
+                       }
+                       ])
+                   }}>
+
+            {marker.map(marker =>
+                <Marker key={marker.time} position={{lat: marker.lat, lng: marker.lng}}  icon={{url:"/gal.png", }}/>
+            )}
+        </GoogleMap>
+
+    </div>;
+}
+
+
